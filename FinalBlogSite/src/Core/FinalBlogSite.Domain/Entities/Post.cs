c7e2daccfcefc5d113e51f3d871c80f4aef9dffb
Your Name <you@example.com>
2024-02-03 14:53:49 +0400

    Initial commit

---
+++ FinalBlogSite/src/Core/FinalBlogSite.Domain/Entities/Post.cs
@@ -0,0 +1,21 @@
+﻿using FinalBlogSite.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalBlogSite.Domain.Entities
+{
+    public class Post:BaseEntity
+    {
+       
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public ICollection<Like>? Likes { get; set; }
+        public ICollection<Comment>? Comments { get; set; }
+
+
+    }
+}
